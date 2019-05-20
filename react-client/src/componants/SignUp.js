acbdea5cb8a4ddab694648effe38bb69d6bda54f
yousefalshabahm <yousefalshabah@hotmail.com>
2019-05-20 13:54:22 +0300

    sloving reouting conflicts

--- react-client/src/componants/SignUp.js
+++ react-client/src/componants/SignUp.js
@@ -1,44 +1,5 @@
-<<<<<<< HEAD
 import React ,{Component} from "react";
-import {NavLink} from "react-router-dom"
-
-class SignUp extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-	render() {
-		return (
-			<div className="sign">
-				<div className="user">
-					<h1 className="hdr">Sign Up ^_^</h1>
-					<form className="form">
-						<div className="form__group">
-							<input type="text" placeholder="Username" className="form__input" />
-						</div>
-
-						<div className="form__group">
-							<input type="email" placeholder="Email" className="form__input" />
-						</div>
-
-						<div className="form__group">
-							<input type="password" placeholder="Password" className="form__input" />
-						</div>
-						<NavLink to="/login">
-						<button className="btn" type="button">
-							Sign Up
-						</button>
-						</NavLink>
-					</form>
-				</div>
-			</div>
-		);
-	}
-}
-
-export default SignUp;
-=======
-import React ,{Component} from 'react';
+import {NavLink} from "react-router-dom";
 
 class SignUp extends Component {
 	constructor(props) {
@@ -91,10 +52,11 @@ class SignUp extends Component {
 						<div className="form__group">
 							<input type="password" placeholder="Password" className="form__input" value={this.state.password} onChange={event => this.onChange(event)} name="password" />
 						</div>
-
+					<NavLink  to="/login">
 						<button className="btn" type="button" onClick={() => this.postSignUp()}>
 							Sign Up
 						</button>
+					</NavLink>
 					</form>
 				</div>
 			</div>
@@ -103,4 +65,3 @@ class SignUp extends Component {
 }
 
 export default SignUp;
->>>>>>> 133302b05379e20356abf6fe5caf32e0c88a0d5f
