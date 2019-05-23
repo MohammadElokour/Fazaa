56c8e3a52054deb7e08b520b98deb9c91427cd02
BasmaAltayeh <basma.altayeh@gmail.com>
2019-05-23 18:24:31 +0300

    payment select2

--- database-mysql/db.js
+++ database-mysql/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 //
-const db = new Sequelize('userdb', 'root', 'root', {
+const db = new Sequelize('userdb', 'root', 'rbk6', {
   host: 'localhost',
   dialect:'mysql'
 });
